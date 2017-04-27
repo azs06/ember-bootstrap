ab3f402d97fdaba172719b1914ab3e018e76c1cf
atiquzzaman_soikat <atik.aiub@gmail.com>
2017-04-27 23:26:34 +0600

    DKW-162: Fixed accept offer form alignment issues.

--- app/components/header-navigation.js
+++ app/components/header-navigation.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement(){
+    this.$('.navbar').affix({
+      offset: {
+        top: 100,
+        bottom: function () {
+          return (this.bottom = $('.footer').outerHeight(true))
+        }
+      }
+    });
+  }
 });
